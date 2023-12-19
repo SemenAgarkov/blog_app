6a5a2466a0a7d79018b9128dc54856336521fb39
Semen <semen.agarkov@gmail.com>
2023-12-19 15:47:12 +0300

    Chapter_3_create_tags_and_filters

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -57,7 +57,7 @@ def post_detail(request, year, month, day, post):
     form = CommentForm()
 
     # Список схожих постов
-    post_tag_ids = post.tags.values_list('id', flat=True)
+    post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids)\
                                     .exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
